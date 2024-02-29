1f60354247acb10e06f4d2bf670f4a549f19e0a7
Naveen Guttedar <naveenguttedar844@gmail.com>
2024-03-01 00:34:54 +0530

    modified

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,1 +1,32 @@
-(()=>{"use strict";function e(e,t,...n){return{type:e,props:{...t,children:n}}}function t(e,n){if("string"==typeof e.type){const r=document.createElement(e.type);for(let t in e.props)"children"!==t&&(r[t]=e.props[t]);e.props.children.forEach((e=>{if("object"==typeof e)t(e,r);else{const t=document.createTextNode(e);r.appendChild(t)}})),n.appendChild(r)}else"function"==typeof e.type&&t(e.type(e.props),n)}const n=(()=>{let n=[],r=0;return{createElement:e,useState:function(e){const t=r,o=n[r]||e;return r++,[o,e=>{n[t]=e}]},useEffect:function(e,t){const o=n[r];let c=!0;o&&(c=t.every(((e,t)=>!Object.is(e,o[t])))),c&&e(),n[r]=t,r++},render:t}})(),r=n.createElement((function(e){const[t,r]=n.useState(1),[o,c]=n.useState("apple");return n.createElement("div",{className:"greeting"},"Hello, ",e.name,"!")}),{name:"World"}),o=document.getElementById("root");n.render(r,o)})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./myReact.js":
+/*!********************!*\
+  !*** ./myReact.js ***!
+  \********************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: [BABEL] /mnt/c/Users/Naveen/Desktop/myReactBabel/myReact.js: .userOptions is not a valid Plugin property\\n    at /mnt/c/Users/Naveen/Desktop/myReactBabel/node_modules/@babel/core/lib/config/validation/plugins.js:58:42\\n    at Array.forEach (<anonymous>)\\n    at validatePluginObject (/mnt/c/Users/Naveen/Desktop/myReactBabel/node_modules/@babel/core/lib/config/validation/plugins.js:48:20)\\n    at /mnt/c/Users/Naveen/Desktop/myReactBabel/node_modules/@babel/core/lib/config/full.js:222:55\\n    at Generator.next (<anonymous>)\\n    at Function.<anonymous> (/mnt/c/Users/Naveen/Desktop/myReactBabel/node_modules/@babel/core/lib/gensync-utils/async.js:21:3)\\n    at Generator.next (<anonymous>)\\n    at step (/mnt/c/Users/Naveen/Desktop/myReactBabel/node_modules/gensync/index.js:261:32)\\n    at evaluateAsync (/mnt/c/Users/Naveen/Desktop/myReactBabel/node_modules/gensync/index.js:291:5)\\n    at Function.errback (/mnt/c/Users/Naveen/Desktop/myReactBabel/node_modules/gensync/index.js:113:7)\\n    at errback (/mnt/c/Users/Naveen/Desktop/myReactBabel/node_modules/@babel/core/lib/gensync-utils/async.js:65:18)\\n    at async (/mnt/c/Users/Naveen/Desktop/myReactBabel/node_modules/gensync/index.js:188:17)\\n    at onFirstPause (/mnt/c/Users/Naveen/Desktop/myReactBabel/node_modules/gensync/index.js:216:13)\\n    at Generator.next (<anonymous>)\\n    at cachedFunction (/mnt/c/Users/Naveen/Desktop/myReactBabel/node_modules/@babel/core/lib/config/caching.js:52:46)\\n    at cachedFunction.next (<anonymous>)\\n    at loadPluginDescriptor (/mnt/c/Users/Naveen/Desktop/myReactBabel/node_modules/@babel/core/lib/config/full.js:259:17)\\n    at loadPluginDescriptor.next (<anonymous>)\\n    at loadPluginDescriptors (/mnt/c/Users/Naveen/Desktop/myReactBabel/node_modules/@babel/core/lib/config/full.js:129:33)\\n    at loadPluginDescriptors.next (<anonymous>)\\n    at /mnt/c/Users/Naveen/Desktop/myReactBabel/node_modules/@babel/core/lib/config/full.js:156:21\\n    at Generator.next (<anonymous>)\\n    at loadFullConfig (/mnt/c/Users/Naveen/Desktop/myReactBabel/node_modules/@babel/core/lib/config/full.js:141:5)\\n    at loadFullConfig.next (<anonymous>)\\n    at transform (/mnt/c/Users/Naveen/Desktop/myReactBabel/node_modules/@babel/core/lib/transform.js:20:44)\\n    at transform.next (<anonymous>)\\n    at step (/mnt/c/Users/Naveen/Desktop/myReactBabel/node_modules/gensync/index.js:269:25)\\n    at /mnt/c/Users/Naveen/Desktop/myReactBabel/node_modules/gensync/index.js:273:13\\n    at async.call.result.err.err (/mnt/c/Users/Naveen/Desktop/myReactBabel/node_modules/gensync/index.js:223:11)\\n    at /mnt/c/Users/Naveen/Desktop/myReactBabel/node_modules/gensync/index.js:37:40\");\n\n//# sourceURL=webpack://myreactbabel/./myReact.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./myReact.js"]();
+/******/ 	
+/******/ })()
+;
