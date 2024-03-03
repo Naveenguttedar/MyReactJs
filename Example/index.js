1b5318d8675c1bef45c78aeb624b8366556f590d
Naveen Guttedar <naveenguttedar844@gmail.com>
2024-03-03 23:40:11 +0530

    coded counter(web-page)

--- Example/index.js
+++ Example/index.js
@@ -1,20 +1,36 @@
-import { default as sayHi, add } from '../add.js';
+import MyReact from '../myReact.js'
+import minus_svg from './minus-3108.svg'
+import plus_svg from './plus-3107.svg'
+import refresh_svg from './refresh-3104.svg'
+import "./style.css"
 
-// import { add } from "./add.js";
-// function Greeting(props) {
-//
-//   const [count, setcount] = MyReact.useState(1);
-//   const [text, setText] = MyReact.useState("apple");
-//   // React.useEffect(()=>console.log("hi there :)"),[text]);
-//   return MyReact.createElement("div", {
-//     className: "greeting",
-//     id: "greet"
-//   }, "Hello, ", props.name, "!");
-// }
-// const element = MyReact.createElement(Greeting, {
-//   name: "World",
-//   age: "25"
-// });
-// const root = document.getElementById('root');
-// MyReact.render(element, root);
-console.log("hello world", add);
+
+// Simple counter app 
+
+function Counter() {
+  const [count, setCount] = MyReact.useState(0);
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const reset = () => setCount(0);
+
+  return (
+    <div className="Container">
+      <h1>Counter </h1>
+      <h1 id="count" className="green">{count}</h1>
+      <div className="buttons">
+         <Button operation={increment} icon={plus_svg}/>
+         <Button operation={reset} icon={refresh_svg}/>
+         <Button operation={decrement} icon={minus_svg}/>
+      </div>
+    </div>
+  );
+}
+function Button({operation,icon}) {
+  return(
+    <div onClick={operation} className="btn">
+      <img src={icon} alt="" />
+    </div>
+  )
+}
+const root = document.getElementById('root');
+MyReact.createRoot(<Counter />, root).render();
