7e0d289b631ec4998525766754f2bd475578a899
Naveen Guttedar <naveenguttedar844@gmail.com>
2024-03-03 22:39:41 +0530

    Added CreateRoot and made support to loop rendering - Created 'CreateRoot' method to store root element and root component. - Modified 'render' to support loop rendering,by checking is it children array. - made some adjustments to 'CreateElement' method.

--- myReact.js
+++ myReact.js
@@ -1,78 +1,112 @@
 const MyReact = (() => {
-  let _hooks = [], idx = 0;
-  function useState(intialValue) {
+  let _hooks = [], idx = 0,_rootComponent=null,_rootContainer=null;
+
+  function useState(initialValue) {
     const _idx = idx;
-    const state = _hooks[idx] || intialValue;
+    const state = _hooks[idx] || initialValue;
     const setState = (newValue) => {
       _hooks[_idx] = newValue;
-    }
+      re_render()
+    };
     idx++;
-    return [state, setState]
+    return [state, setState];
   }
-  function useEffect(cb, dparr) {
-    const oldDparr = _hooks[idx];
+
+  function useEffect(cb, depArray) {
+    const oldDepArray = _hooks[idx];
     let hasChanged = true;
-    if (oldDparr) {
-      hasChanged = dparr.every((item, i) => {
-        return !Object.is(item, oldDparr[i]);
-      })
+    if (oldDepArray) {
+      hasChanged = depArray.every((item, i) => {
+        return !Object.is(item, oldDepArray[i]);
+      });
     }
-    if (hasChanged) cb()
-    _hooks[idx] = dparr;
+    if (hasChanged) cb();
+    _hooks[idx] = depArray;
     idx++;
   }
-  // source https://pomb.us/build-your-own-react/
+
   function createElement(type, props, ...children) {
     return {
       type,
       props: {
         ...props,
-        children
+        children: children.map(child =>
+          typeof child === 'object' ? child : createTextElement(child)
+        )
       }
     };
   }
-  function render(element, container) {
-    if (typeof element.type === 'string') {
-      const domElement = document.createElement(element.type);
-      for (let prop in element.props) {
-        if (prop !== 'children') {
-          domElement[prop] = element.props[prop];
-        }
+
+  function createTextElement(text) {
+    return {
+      type: 'TEXT_ELEMENT',
+      props: {
+        nodeValue: text,
+        children: []
       }
-
-      element.props.children.forEach(child => {
-        if (typeof child === 'object') {
-          render(child, domElement);
-        } else {
-          const textNode = document.createTextNode(child);
-          domElement.appendChild(textNode);
-        }
-      });
-
-      container.appendChild(domElement);
-    } else if (typeof element.type === 'function') {
-      const component = element.type(element.props);
-      render(component, container);
+    };
+  }
+  function createRoot(element,container){
+    _rootComponent=element;
+    _rootContainer=container;
+    return{
+      render:()=>render(_rootComponent,_rootContainer)
     }
   }
-  return { createElement, useState, useEffect, render }
-})()
-// function Greeting(props) {
-//     const [count, setcount] = MyReact.useState(1);
-//     const [text, setText] = MyReact.useState("apple");
-//     // React.useEffect(()=>console.log("hi there :)"),[text]);
-//     return (
-//         <div className="greeting" id="greet">
-//             Hello, {props.name}!
-//         </div>
-//     )
-//
-// }
-// const element = <Greeting name="World" age="25" />;
-// const root = document.getElementById('root');
-// MyReact.render(element, root);
-function sayHi(user) {
-  alert(`Hello, ${user}!`);
-}
-export { sayHi as default };
+  function render(element, container) {
+    if (typeof element === 'string' || typeof element === 'number') {
+      container.appendChild(document.createTextNode(String(element)));
+      return;
+    }
+
+    if (Array.isArray(element)) {
+      element.forEach((child) => {
+        render(child, container);
+      });
+      return;
+    }
+
+    if (typeof element === 'object' && element !== null) {
+      if (element.type === 'TEXT_ELEMENT') {
+        const textNode = document.createTextNode(element.props.nodeValue);
+        container.appendChild(textNode);
+        return;
+      };
+      if (typeof element.type === 'string') {
+        const domElement = document.createElement(element.type);
+
+        for (let prop in element.props) {
+          if (prop === 'onClick') {
+            domElement.addEventListener('click', element.props[prop]);
+          } else if (prop !== 'children') {
+            domElement[prop] = element.props[prop];
+          }
+        }
+
+        if (element.props.children) {
+          element.props.children.forEach((child) => {
+            render(child, domElement);
+          });
+        }
+
+        container.appendChild(domElement);
+      } else if (typeof element.type === 'function') {
+        const component = element.type(element.props);
+        render(component, container);
+      }
+    }
+  }
+
+  function re_render(){
+    idx = 0; // Reset index for new render
+    const rootContainer = _rootContainer || document.getElementById('root');
+    while (rootContainer.firstChild) {
+      rootContainer.removeChild(rootContainer.firstChild);
+    }
+    render(_rootComponent, rootContainer);
+  }
+  return { createElement, useState, useEffect, createRoot,re_render  };
+})();
+
+export default MyReact;
 
